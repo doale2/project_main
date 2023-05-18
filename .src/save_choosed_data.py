8b3c78966ba6826a3b4568e82a1909d2be28dee9
김규식 <kimq6@hanyang.ac.kr>
2023-05-18 09:11:54 +0900

    아무것도 선택하지 않으면 전체선택됨

--- .src/save_choosed_data.py
+++ .src/save_choosed_data.py
@@ -6,14 +6,25 @@ def save_choosed_data(self):
     select_lot_name = []
     select_wafer_name = []
     select_coordinate_name = []
-    for index in self.select_listbox1.curselection():
-        select_lot_name.append(self.lot_list[index])
 
-    for index in self.select_listbox2.curselection():
-        select_wafer_name.append(self.wafer_list[index])
+    # 선택한 것을 추가, 아무것도 선택하지 않으면 전체선택
+    if self.select_listbox1.curselection() == ():
+        select_lot_name = self.lot_list
+    else:
+        for index in self.select_listbox1.curselection():
+            select_lot_name.append(self.lot_list[index])
 
-    for index in self.select_listbox3.curselection():
-        select_coordinate_name.append(self.coordinate_list[index])
+    if self.select_listbox2.curselection() == ():
+        select_wafer_name = self.wafer_list
+    else:
+        for index in self.select_listbox2.curselection():
+            select_wafer_name.append(self.wafer_list[index])
+
+    if self.select_listbox3.curselection() == ():
+        select_coordinate_name = self.coordinate_list
+    else:
+        for index in self.select_listbox3.curselection():
+            select_coordinate_name.append(self.coordinate_list[index])
 
     # 선택한 값을 dat_dict에서 찾아서 키값인 path를 select_path_list 에 저장
     select_path_list = []
