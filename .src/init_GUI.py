7a34f4a6f776de6952896123f020a9da623bc826
yujeongkiim <govldjsgovl@hanyang.ac.kr>
2023-05-28 01:50:08 +0900

    gui

--- .src/init_GUI.py
+++ .src/init_GUI.py
@@ -28,9 +28,9 @@ def init_GUI(self):
     self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
     # 왼쪽에 위젯 버튼
     self.checkbox_1 = customtkinter.CTkCheckBox(master=self.sidebar_frame, text="save png", variable=method_var1)
-    self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="w")
+    self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="nw")
     self.checkbox_2 = customtkinter.CTkCheckBox(master=self.sidebar_frame, text="save csv", variable=method_var2)
-    self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="w")
+    self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="nw")
     # 왼쪽 아래 라이트, 다크 모드
     self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
     self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
@@ -49,8 +49,8 @@ def init_GUI(self):
     self.scaling_optionmenu.set("100%")
 
     # 왼 쪽에서 두번째 줄 tab 창(lot, wafer, location, date)
-    self.tabview = customtkinter.CTkTabview(self, width=300, height=500)
-    self.tabview.grid(row=0, column=1, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
+    self.tabview = customtkinter.CTkTabview(self)
+    self.tabview.grid(row=0, rowspan=3, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
     self.tabview.add("Lot ID")
     self.tabview.add("Wafer ID")
     self.tabview.add("Location")
@@ -61,16 +61,16 @@ def init_GUI(self):
     self.tabview.tab("Date").grid_columnconfigure(0, weight=1)
 
     self.num_files_label = customtkinter.CTkLabel(self, text="Number of Files: 0")
-    self.num_files_label.grid(row=1, column=2, padx=5, pady=5, sticky="w")
+    self.num_files_label.grid(row=3, column=1, sticky="n", pady=10)
 
     # set scale
     self.sidebar_button_1 = customtkinter.CTkButton(self, command=self.save_choosed_data, text="set scale")
-    self.sidebar_button_1.grid(row=1, column=2, padx=20, pady=10, sticky="e")
+    self.sidebar_button_1.grid(row=3, column=1,sticky="ne", pady=10)
 
     # progress bar
     self.progress_value = customtkinter.DoubleVar()
     self.progress_ratio_label = customtkinter.CTkLabel(self, text="Progress ratio:  0%")
-    self.progress_ratio_label.grid(row=3, column=1, padx=20, pady=20, sticky="ws")
+    self.progress_ratio_label.grid(row=3, column=1, padx=30, pady=0, sticky="ws")
     self.progress_bar = customtkinter.CTkProgressBar(self, determinate_speed=0, mode="determinate",
                                                      variable=self.progress_value)
     self.progress_bar.grid(row=4, column=1, columnspan=2, padx=20, pady=20, sticky="nsew")
@@ -80,6 +80,7 @@ def init_GUI(self):
     self.wafer_list = []
     self.coordinate_list = []
     self.date_list = []
+
 
     # tabview에 글씨 넣기
     self.select_listbox1 = tk.Listbox(self.tabview.tab("Lot ID"), width=30, height=15, selectmode='multiple',
@@ -98,9 +99,10 @@ def init_GUI(self):
                                       exportselection=0)
     self.select_listbox4.grid(row=1, column=0, padx=5, pady=5)
 
+
     # selected xml files 표기용 박스
     self.textbox = customtkinter.CTkTextbox(self, width=250, height=100, state='disabled')
-    self.textbox.grid(row=0, column=3, padx=(20, 0), pady=(20, 0), sticky="nsew", rowspan=3)
+    self.textbox.grid(row=0, rowspan=3, column=2, columnspan=2, padx=(20, 20), pady=(20, 0), sticky="nsew")
 
     # 오른쪽 아래 analyze button
     self.analyze_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2,
@@ -108,7 +110,8 @@ def init_GUI(self):
                                                     command=lambda: self.analyze_data([
                                                         'png' if method_var1.get() else None,
                                                         'csv' if method_var2.get() else None]))
-    self.analyze_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
+    self.analyze_button_1.grid(row=3, column=3, padx=(20, 20), pady=(10, 20), sticky="nsew")
+
     # 오른쪽 아래 exit button
     self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2,
                                                  text_color=("gray10", "#DCE4EE"), text="exit",
